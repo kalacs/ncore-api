b25014bed4edbd7e9b00d2be2e1d6ebd39744715
Zoltan Kakuk <kakuk.zoltan@e-vista.hu>
2020-03-22 11:28:05 +0100

    Add got plugins

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const makeScraper = require('./lib/scraper');
 
 // get latest movies
 (async () => {
-  const scraper = makeScraper({ username: '', password: '', type: 'ncore' });
-  const movies = await scraper.getMovies();
-  console.dir(movies);
+  try {
+    const scraper = makeScraper({ username: '', password: '', type: 'ncore' });
+    const movies = await scraper.getMovies();
+    //    console.dir(movies);
+  } catch (error) {
+    console.dir(error);
+  }
 })();
