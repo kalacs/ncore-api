4ccbecfd83328f9cfd271cbb42c87946a916f882
Zoltan Kakuk <kakuk.zoltan@e-vista.hu>
2020-03-23 22:10:57 +0100

    Handling cookie properly

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 /* eslint-disable no-console */
+require('dotenv').config();
 const makeScraper = require('./lib/scraper');
 
 // get latest movies
 (async () => {
   try {
-    const scraper = makeScraper({ username: '', password: '', type: 'ncore' });
+    const scraper = makeScraper({
+      username: process.env.NCORE_NICK,
+      password: process.env.NCORE_PASSHASH,
+      type: 'ncore',
+    });
     const movies = await scraper.getMovies();
-    //    console.dir(movies);
+    console.dir(movies);
   } catch (error) {
     console.dir(error);
   }
