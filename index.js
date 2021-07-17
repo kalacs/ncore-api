117b108b95da8f524083e0d6a569ebd7b3154a0f
Zoltan Kakuk <kakuk.zoltan@e-vista.hu>
2021-07-18 01:52:31 +0200

    Fix bugs and make auto login feature

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/* eslint-disable no-console */
-const makeScraper = require('./lib/scraper');
+const createScraper = require('./lib/scraper');
 
-/*
-const scraper = makeScraper({
-  username: process.env.NCORE_NICK,
-  password: process.env.NCORE_PASSHASH,
-  type: 'ncore',
-});
-*/
-module.exports = makeScraper;
+module.exports = createScraper;
